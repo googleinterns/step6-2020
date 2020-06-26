af21e42830adf33704fc13f9eab3a9e1608051b7
Lukas Berglund <lukastberglund@gmail.com>
2020-06-26 18:00:52 +0000

    Add license to .js files and replace double quotes with single quotes as per google styleguide

--- capstone/src/main/webapp/js/profile.js
+++ capstone/src/main/webapp/js/profile.js
@@ -1,13 +1,27 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Toggle between view and edit profile options.
 function toggleProfile() {
-  var viewProfile = document.getElementById("view-profile-section");
-  var editProfile = document.getElementById("edit-profile-section");
+  var viewProfile = document.getElementById('view-profile-section');
+  var editProfile = document.getElementById('edit-profile-section');
 
-  if (viewProfile.style.display == "block") {
-    viewProfile.style.display = "none";
-    editProfile.style.display = "block";
+  if (viewProfile.style.display == 'block') {
+    viewProfile.style.display = 'none';
+    editProfile.style.display = 'block';
   } else {
-    editProfile.style.display = "none";
-    viewProfile.style.display = "block";
+    editProfile.style.display = 'none';
+    viewProfile.style.display = 'block';
   }
 }
