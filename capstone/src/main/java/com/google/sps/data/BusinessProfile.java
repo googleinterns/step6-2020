52ec3dc67a5b719f4db8d785a19fb7fa0196cb96
Eashan Bhatt <eash130@gmail.com>
2020-06-30 17:48:59 +0000

    Implement BusinessServlet

---
+++ capstone/src/main/java/com/google/sps/data/BusinessProfile.java
@@ -0,0 +1,17 @@
+package com.google.sps.data;
+
+public final class BusinessProfile {
+
+  private final String name;
+  private final String email;
+  private final String bio;
+  private final String location;
+
+  public BusinessProfile(String name, String email, String bio, String location) {
+    this.name = name;
+    this.email = email;
+    this.bio = bio;
+    this.location = location;
+  }
+
+}
