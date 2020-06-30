bd95fd7ee4c243ed01c24e07819d52b28e6af49a
Winnie Wan <39419317+winniewan@users.noreply.github.com>
2020-06-30 12:20:11 -0400

    Revert "Add Java lint and ESLint"

--- capstone/src/main/java/com/google/sps/App.java
+++ capstone/src/main/java/com/google/sps/App.java
@@ -14,7 +14,10 @@
 
 package com.google.sps;
 
-/** Hello world! */
+/**
+ * Hello world!
+ *
+ */
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
--- capstone/src/test/java/com/google/sps/AppTest.java
+++ capstone/src/test/java/com/google/sps/AppTest.java
@@ -18,9 +18,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/** Unit test for simple App. */
+/**
+ * Unit test for simple App.
+ */
 public class AppTest {
-  /** Rigorous Test :-) */
+  /**
+    * Rigorous Test :-)
+    */
   @Test
   public void shouldAnswerWithTrue() {
     assertTrue(true);
