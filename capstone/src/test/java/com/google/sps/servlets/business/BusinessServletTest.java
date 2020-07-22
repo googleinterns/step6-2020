8b2eb8c60939c3be974765612b9271750680e06b
Winnie <winniewan6@gmail.com>
2020-07-22 15:16:28 +0000

    Add it to business servlet

--- capstone/src/test/java/com/google/sps/servlets/business/BusinessServletTest.java
+++ capstone/src/test/java/com/google/sps/servlets/business/BusinessServletTest.java
@@ -14,7 +14,21 @@
 
 package com.google.sps.servlets;
 
+import static com.google.sps.data.ProfileDatastoreUtil.ABOUT_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.BIO_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.CALENDAR_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.GEO_PT_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.IS_BUSINESS_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.LAT_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.LOCATION_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.LONG_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.NAME_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.NO;
 import static com.google.sps.data.ProfileDatastoreUtil.PROFILE_TASK_NAME;
+import static com.google.sps.data.ProfileDatastoreUtil.STORY_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.SUPPORT_PROPERTY;
+import static com.google.sps.data.ProfileDatastoreUtil.YES;
+
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
@@ -22,6 +36,7 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.GeoPt;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
@@ -58,6 +73,8 @@ public class BusinessServletTest {
   private static final String NAME = "Pizzeria";
   private static final String NO_NAME = null;
   private static final String LOCATION = "Mountain View, CA";
+  private static final String LAT = "45.0";
+  private static final String LONG = "45.0";
   private static final String BIO = "This is my business bio.";
   private static final String STORY = "The pandemic has affected my business in X many ways.";
   private static final String ABOUT = "Here is the Pizzeria's menu.";
@@ -75,6 +92,7 @@ public class BusinessServletTest {
   private BusinessServlet servlet;
   private DatastoreService datastore;
   private StringWriter servletResponseWriter;
+  private GeoPt GEO_PT;
 
   @Before
   public void setUp() throws Exception {
@@ -95,6 +113,7 @@ public class BusinessServletTest {
     servletResponseWriter = new StringWriter();
     doReturn(new PrintWriter(servletResponseWriter)).when(response).getWriter();
     servlet = new BusinessServlet();
+    GEO_PT = new GeoPt(Float.parseFloat(LAT), Float.parseFloat(LONG));
   }
 
   @After
@@ -105,13 +124,14 @@ public class BusinessServletTest {
   // Set static variables to entity.
   public Entity setUserProfileData() {
     Entity ent = new Entity(PROFILE_TASK_NAME, USER_ID);
-    ent.setProperty("name", NAME);
-    ent.setProperty("location", LOCATION);
-    ent.setProperty("bio", BIO);
-    ent.setProperty("story", STORY);
-    ent.setProperty("about", ABOUT);
-    ent.setProperty("calendarEmail", EMAIL);
-    ent.setProperty("support", SUPPORT);
+    ent.setProperty(NAME_PROPERTY, NAME);
+    ent.setProperty(LOCATION_PROPERTY, LOCATION);
+    ent.setProperty(GEO_PT_PROPERTY, GEO_PT);
+    ent.setProperty(BIO_PROPERTY, BIO);
+    ent.setProperty(STORY_PROPERTY, STORY);
+    ent.setProperty(ABOUT_PROPERTY, ABOUT);
+    ent.setProperty(CALENDAR_PROPERTY, EMAIL);
+    ent.setProperty(SUPPORT_PROPERTY, SUPPORT);
 
     return ent;
   }
@@ -154,7 +174,7 @@ public class BusinessServletTest {
     when(request.getPathInfo()).thenReturn(PATHINFO);
 
     Entity fakeBusiness = setUserProfileData();
-    fakeBusiness.setProperty("isBusiness", "No");
+    fakeBusiness.setProperty(IS_BUSINESS_PROPERTY, NO);
     datastore.put(fakeBusiness);
 
     servlet.doGet(request, response);
@@ -173,7 +193,7 @@ public class BusinessServletTest {
     when(request.getPathInfo()).thenReturn(PATHINFO);
 
     Entity validBusiness = setUserProfileData();
-    validBusiness.setProperty("isBusiness", "Yes");
+    validBusiness.setProperty(IS_BUSINESS_PROPERTY, YES);
     datastore.put(validBusiness);
 
     servlet.doGet(request, response);
@@ -223,14 +243,16 @@ public class BusinessServletTest {
    */
   @Test
   public void userEditProfileAddToDatastore() throws Exception {
-    when(request.getParameter("isBusiness")).thenReturn("Yes");
-    when(request.getParameter("name")).thenReturn(NAME);
-    when(request.getParameter("location")).thenReturn(LOCATION);
-    when(request.getParameter("bio")).thenReturn(BIO);
-    when(request.getParameter("story")).thenReturn(STORY);
-    when(request.getParameter("about")).thenReturn(ABOUT);
-    when(request.getParameter("calendarEmail")).thenReturn(EMAIL);
-    when(request.getParameter("support")).thenReturn(SUPPORT);
+    when(request.getParameter(IS_BUSINESS_PROPERTY)).thenReturn("Yes");
+    when(request.getParameter(NAME_PROPERTY)).thenReturn(NAME);
+    when(request.getParameter(LOCATION_PROPERTY)).thenReturn(LOCATION);
+    when(request.getParameter(LAT_PROPERTY)).thenReturn(LAT);
+    when(request.getParameter(LONG_PROPERTY)).thenReturn(LONG);
+    when(request.getParameter(BIO_PROPERTY)).thenReturn(BIO);
+    when(request.getParameter(STORY_PROPERTY)).thenReturn(STORY);
+    when(request.getParameter(ABOUT_PROPERTY)).thenReturn(ABOUT);
+    when(request.getParameter(CALENDAR_PROPERTY)).thenReturn(EMAIL);
+    when(request.getParameter(SUPPORT_PROPERTY)).thenReturn(SUPPORT);
 
     servlet.doPost(request, response);
 
@@ -239,14 +261,15 @@ public class BusinessServletTest {
 
     Entity capEntity = datastore.get(userKey);
 
-    Assert.assertEquals(capEntity.getProperty("isBusiness"), "Yes");
-    Assert.assertEquals(capEntity.getProperty("name"), NAME);
-    Assert.assertEquals(capEntity.getProperty("location"), LOCATION);
-    Assert.assertEquals(capEntity.getProperty("bio"), BIO);
-    Assert.assertEquals(capEntity.getProperty("story"), STORY);
-    Assert.assertEquals(capEntity.getProperty("about"), ABOUT);
-    Assert.assertEquals(capEntity.getProperty("calendarEmail"), EMAIL);
-    Assert.assertEquals(capEntity.getProperty("support"), SUPPORT);
+    Assert.assertEquals(capEntity.getProperty(IS_BUSINESS_PROPERTY), YES);
+    Assert.assertEquals(capEntity.getProperty(NAME_PROPERTY), NAME);
+    Assert.assertEquals(capEntity.getProperty(LOCATION_PROPERTY), LOCATION);
+    Assert.assertEquals(capEntity.getProperty(GEO_PT_PROPERTY), GEO_PT);
+    Assert.assertEquals(capEntity.getProperty(BIO_PROPERTY), BIO);
+    Assert.assertEquals(capEntity.getProperty(STORY_PROPERTY), STORY);
+    Assert.assertEquals(capEntity.getProperty(ABOUT_PROPERTY), ABOUT);
+    Assert.assertEquals(capEntity.getProperty(CALENDAR_PROPERTY), EMAIL);
+    Assert.assertEquals(capEntity.getProperty(SUPPORT_PROPERTY), SUPPORT);
   }
 
   /**
@@ -255,8 +278,8 @@ public class BusinessServletTest {
    */
   @Test
   public void nonBusinessUserEditProfileAddToDatastore() throws Exception {
-    when(request.getParameter("isBusiness")).thenReturn("No");
-    when(request.getParameter("name")).thenReturn(NAME);
+    when(request.getParameter(IS_BUSINESS_PROPERTY)).thenReturn(NO);
+    when(request.getParameter(NAME_PROPERTY)).thenReturn(NAME);
 
     servlet.doPost(request, response);
 
