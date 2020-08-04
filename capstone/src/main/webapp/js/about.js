a66283938be4e14722b1aa82b5168396916e8b4d
Winnie <winniewan6@gmail.com>
2020-08-04 22:13:52 +0000

    fix navigation bug

---
+++ capstone/src/main/webapp/js/about.js
@@ -0,0 +1,26 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { buildElement, getJsonObject, setLoginOrLogoutUrl, setProfileUrl } from '/js/util.js';
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  // Check whether it's a new user.
+  fetch('/check_new_user'); 
+
+  // Get login status of user to display on nav bar.
+  setLoginOrLogoutUrl();
+  
+  // Update and set profile nav link.
+  setProfileUrl();
+})
