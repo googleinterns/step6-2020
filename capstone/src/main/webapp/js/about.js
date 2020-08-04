0ba3de8bbad3d942cb415dea0ec2af748f271b3b
Winnie <winniewan6@gmail.com>
2020-08-04 22:24:44 +0000

    remove access imports

--- capstone/src/main/webapp/js/about.js
+++ capstone/src/main/webapp/js/about.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { buildElement, getJsonObject, setLoginOrLogoutUrl, setProfileUrl } from '/js/util.js';
+import { setLoginOrLogoutUrl, setProfileUrl } from '/js/util.js';
 
 window.addEventListener('DOMContentLoaded', (event) => {
   // Get login status of user to display on nav bar.
